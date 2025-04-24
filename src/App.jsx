c944cbbdef4b19dd450596ffe556add903e0cc0d
Soye Usoroh <usorohsoye@gmail.com>
2025-04-24 15:42:07 +0100

    'Remove un-necessary imports in App.jsx'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import Main from "./components/Main";
 import Taskbar from "./components/Taskbar";
 import NavBar from "./components/NavBar";
-import QuoteBlock from "./components/QuoteBlock";
 import HomePage from "./components/HomePage";
 
 function App() {
