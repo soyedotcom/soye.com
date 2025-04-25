49ff9545fc45e4b11a70c4eea044e1026b96af32
Soye Usoroh <usorohsoye@gmail.com>
2025-04-25 14:19:53 +0100

    Remove text change in CodeBlock.jsx

--- src/components/Codeblock.jsx
+++ src/components/Codeblock.jsx
@@ -1,25 +1,11 @@
-import { useState, useEffect, useRef } from "react";
 import "./styles/Codeblock.css";
 
 const Codeblock = () => {
-  const index = useRef(0);
-  const roles = ["Programmer", "Artist", "Gamer", "Student"];
-  const [role, setrole] = useState(roles[3]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      index.current = (index.current + 1) % roles.length;
-      setrole(roles[index.current]);
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className="code-container">
       <p className="code-text">
         &#123;name = "Soye", role = "
-        <span className="code-highlight">{role}</span>
+        <span className="code-highlight">Programmer</span>
         "&#125;
       </p>
     </div>
