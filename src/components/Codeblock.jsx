67bd1f418819ec520bf98f61d5d2b570712fab78
Soye Usoroh <usorohsoye@gmail.com>
2025-04-25 11:27:00 +0100

    Added text change in Codeblock.jsx

--- src/components/Codeblock.jsx
+++ src/components/Codeblock.jsx
@@ -1,11 +1,25 @@
+import { useState, useEffect, useRef } from "react";
 import "./styles/Codeblock.css";
 
 const Codeblock = () => {
+  const index = useRef(0);
+  const roles = ["Programmer", "Artist", "Gamer", "Creative Consumer"];
+  const [role, setrole] = useState(roles[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      index.current = (index.current + 1) % roles.length;
+      setrole(roles[index.current]);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="code-container">
       <p className="code-text">
         &#123;name = "Soye", role = "
-        <span className="code-highlight">Programmer</span>
+        <span className="code-highlight">{role}</span>
         "&#125;
       </p>
     </div>
