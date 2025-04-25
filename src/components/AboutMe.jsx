3c40d35caef0c536af0b8d3dce7475b05e3b2604
Soye Usoroh <usorohsoye@gmail.com>
2025-04-25 18:10:41 +0100

    Add properties to AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,2 +1,33 @@
-const AboutMe = () => {};
-export default AboutMe;
+import AboutBlock from "./AboutBlock";
+import Codeblock from "./Codeblock";
+import HeadingWithPageTitle from "./HeadingWithPageTitle";
+import QuoteBlock from "./QuoteBlock";
+
+import "./styles/HomePage.css";
+
+const HomePage = () => {
+  return (
+    <main className="web-section">
+      <div>
+        <HeadingWithPageTitle
+          currentSect="ABOUT ME"
+          sectTitle="whoami"
+          subHeading="Hi, I'm Soye Usoroh -- A computer engineering undergrad, builder of cool web things and dreamer of digital worlds."
+        />
+        <Codeblock
+          codeText={
+            <>
+              soye
+              <span className="code-highlight">@mindofsoye:~$ </span>
+              cat aboutMe.txt
+            </>
+          }
+        />
+      </div>
+      <div>
+        <AboutBlock />
+      </div>
+    </main>
+  );
+};
+export default HomePage;
