a30812ffdf3c19750996865edf9fd340611e888d
Soye Usoroh <usorohsoye@gmail.com>
2025-04-28 12:24:04 +0100

    Add gridArea to BentoBox

--- src/components/ProjectBentoBox.jsx
+++ src/components/ProjectBentoBox.jsx
@@ -3,14 +3,14 @@ import "./styles/ProjectBentoBox.css";
 const ProjectBentoBox = () => {
   return (
     <main className="project-container">
-      <section className="box"></section>
-      <section className="box"></section>
-      <section className="box"></section>
-      <section className="box"></section>
-      <section className="box"></section>
-      <section className="box"></section>
-      <section className="box"></section>
-      <section className="box"></section>
+      <section style={{ gridArea: "box-1" }} className="box"></section>
+      <section style={{ gridArea: "box-2" }} className="box"></section>
+      <section style={{ gridArea: "box-3" }} className="box"></section>
+      <section style={{ gridArea: "box-4" }} className="box"></section>
+      <section style={{ gridArea: "box-5" }} className="box"></section>
+      <section style={{ gridArea: "box-6" }} className="box"></section>
+      <section style={{ gridArea: "box-7" }} className="box"></section>
+      <section style={{ gridArea: "box-8" }} className="box"></section>
     </main>
   );
 };
