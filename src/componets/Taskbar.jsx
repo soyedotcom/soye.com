a6463e754c7eceed7ddbd9d0882a85d0eef2e12b
Soye Usoroh <usorohsoye@gmail.com>
2025-05-06 10:32:09 +0100

    Taskbar.jsx finalized

--- src/componets/Taskbar.jsx
+++ src/componets/Taskbar.jsx
@@ -2,11 +2,13 @@ import "../styles/Taskbar.css";
 
 const Taskbar = () => {
   return (
-    <header className="taskbar">
-      <a href="/index.html" className="logo">
-        M.O.S
-      </a>
-    </header>
+    <div className="taskbar-container">
+      <header className="taskbar">
+        <a href="/index.html" className="logo">
+          M.O.S
+        </a>
+      </header>
+    </div>
   );
 };
 export default Taskbar;
