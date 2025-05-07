6585728598600ecb36eafd5e77a452908ee5c71b
Soye Usoroh <usorohsoye@gmail.com>
2025-05-07 10:13:56 +0100

    Add BentoBox.jsx structure

--- src/components/BentoBox.jsx
+++ src/components/BentoBox.jsx
@@ -1,2 +1,17 @@
-const BentoBox = () => {};
+import "../styles/BentoBox.css";
+
+const BentoBox = () => {
+  return (
+    <main className="project-container">
+      <section style={{ gridArea: "box-1" }} className="box"></section>
+      <section style={{ gridArea: "box-2" }} className="box"></section>
+      <section style={{ gridArea: "box-3" }} className="box"></section>
+      <section style={{ gridArea: "box-4" }} className="box"></section>
+      <section style={{ gridArea: "box-5" }} className="box"></section>
+      <section style={{ gridArea: "box-6" }} className="box"></section>
+      <section style={{ gridArea: "box-7" }} className="box"></section>
+      <section style={{ gridArea: "box-8" }} className="box"></section>
+    </main>
+  );
+};
 export default BentoBox;
