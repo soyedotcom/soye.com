c0bd60a07dc6d79032d066067d3bd016ba0a1d9c
Soye Usoroh <usorohsoye@gmail.com>
2025-05-07 19:06:44 +0100

    Position Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,23 @@
-import '../styles/Navbar.css'
+import "../styles/Navbar.css";
 
-const Navbar = () => {}
-export default Navbar
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <a href="#home">
+          <li className="section-link">Home</li>
+        </a>
+        <a href="#about">
+          <li className="section-link">About Me</li>
+        </a>
+        <a href="#projects">
+          <li className="section-link">Projects</li>
+        </a>
+        <a href="#contact">
+          <li className="section-link">Say Hello</li>
+        </a>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
