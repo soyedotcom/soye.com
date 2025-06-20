35970adaec98915454db25056f1fb96466d0597c
Soye Usoroh <usorohsoye@gmail.com>
2025-06-20 09:22:43 +0100

    Create refresh branch and remove all non-git files

--- src/PageNotFound.jsx
+++
@@ -1,7 +0,0 @@
-import { Link } from "react-router-dom";
-import "../src/PageNotFound.css";
-
-const PageNotFound = () => {
-  <h1>Page Does Not Exist</h1>;
-};
-export default PageNotFound;
--- src/components/AboutMeTxt.jsx
+++
@@ -1,2 +0,0 @@
-const AboutMeTxt = () => {};
-export default AboutMeTxt;
